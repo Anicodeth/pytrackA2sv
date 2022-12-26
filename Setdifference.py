af3dbe7841c5d7bc8db704a531af6994bd735fec
Anicodeth <105945598+Anicodeth@users.noreply.github.com>
2022-12-26 07:12:15 -0800

    Create Setdifference.py

---
+++ Setdifference.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n=int(input())
+lis1=set(input().split())
+m=int(input())
+lis2=set(input().split())
+print(len(lis1.difference(lis2)))
+
+
+    
